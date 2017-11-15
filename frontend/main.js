2bd9ab984b4bf09dec3e140ba0595a0acaa552f4
Nitin Surana <surana.nitin1990@gmail.com>
2017-11-15 14:37:45 -0800

    Updated the character query

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,6 @@
 'use strict'
-const SPARQL_ENDPOINT = 'http://localhost:3030/new/query';
+const SPARQL_ENDPOINT = 'http://localhost:3030/nov15_2/query';
+const QUERY_PREFIX = "prefix ns2: <http://www.semanticweb.org/gurleenk/ontologies/2017/10/raw-ontology#>";
 
 function getSubject(str) {
     let regex = new RegExp('#(\\w+)$');
@@ -22,30 +23,37 @@ $(() => {
 
     $("#findCharacters").click(() => {
         let resultsTable = $('.age-character-query-container table.results tbody');
+        let characterTag = $("#characterTags").val();
+        let ageRating = $("#ageRating").val();
         $.ajax({
             url: SPARQL_ENDPOINT,
             method: 'post',
             data: {
-                query: "SELECT ?subject ?predicate ?object " +
-                "      WHERE { " +
-                "       ?subject ?predicate ?object " +
-                "} " +
-                " LIMIT 25"
+                query: QUERY_PREFIX + " SELECT   ?characterName  ?loveCount " +
+                " WHERE { " +
+                "      ?subject ns2:hasCharacter ?object. " +
+                " ?object ns2:hasName ?characterName; " +
+                " ns2:hasTag \"" + characterTag + "\"; " +
+                " ns2:hasLoveRank ?loveCount; " +
+                " ns2:hasAgeRating ns2:" + ageRating + ". " +
+                "  " +
+                " } ORDER BY DESC (?loveCount) " +
+                " LIMIT 10"
             }
         }).then(response => {
             console.log(response);
             response.results.bindings.forEach(o => {
-                let subject = getSubject(o.subject.value);
-                let object = getSubject(o.object.value);
+                let subject = getSubject(o.characterName.value);
+                let object = getSubject(o.loveCount.value);
                 let $tr = $("<tr>");
                 let $td_one = $("<td>", {
-                    html: 'nitin'
+                    html: subject
                 });
                 let $td_two = $("<td>", {
                     html: 'nitin'
                 });
                 let $td_three = $("<td>", {
-                    html: 'nitin'
+                    html: object
                 });
                 $tr.append($td_one);
                 $tr.append($td_two);
